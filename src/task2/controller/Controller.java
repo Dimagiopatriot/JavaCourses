7656ff7ee680f6a0d4a2efcac7d52cff5e8fd042
Dimagio <sdmitriy3758@gmail.com>
2017-07-24 02:03:38 +0300

    Delete model functionality

--- src/task2/controller/Controller.java
+++ src/task2/controller/Controller.java
@@ -1,15 +1,15 @@
 package task2.controller;
 
+import task2.model.Constants;
 import task2.model.Model;
 import task2.view.View;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
  * task2.Controller
  *
- * v 1.0
+ * v 1.0.1
  *
  * 20/07/2017
  *
@@ -21,13 +21,6 @@ public class Controller {
     private Model model;
     private View view;
 
-    //Range of search
-    private int maxOfRange = 100;
-    private int minOfRange = 0;
-
-    //random number
-    private int number;
-
     public Controller(Model model, View view) {
         this.model = model;
         this.view = view;
@@ -36,8 +29,8 @@ public class Controller {
     /**
      * Set value for number
      * */
-    private void random() {
-        number = (int) Math.round(Math.random() * 100);
+    public int random(int minRange, int maxRange) {
+         return (int) Math.round(Math.random() * (maxRange - 1)) + minRange + 1;
     }
 
     /**
@@ -45,80 +38,37 @@ public class Controller {
      * */
     public void process() {
         Scanner scanner = new Scanner(System.in);
-        random();
+        model.setSearchableNumber(random(Constants.RANGE_MIN, Constants.RANGE_MAX));
         getValueFromScanner(scanner);
     }
 
     /**
      * Get searchable value from scanner
      * */
-    private void getValueFromScanner(Scanner scanner) {
-        view.showInputMessage(minOfRange, maxOfRange);
-        while (!scanner.hasNext(String.valueOf(number))) {
+    public void getValueFromScanner(Scanner scanner) {
+        view.showInputMessage(model.getMinOfRange(), model.getMaxOfRange());
+        while (!scanner.hasNext(String.valueOf(model.getSearchableNumber()))) {
             checkInputType(scanner);
         }
 
         int result = scanner.nextInt();
         model.addNumberToStatistic(result);
-        model.setSearchNumber(result);
-        view.showResult(model.toString());
+        view.showMessage(model.toString());
     }
 
     /**
      * Check if input type is integer
      * */
-    private void checkInputType(Scanner scanner) {
+    public void checkInputType(Scanner scanner) {
         if (!scanner.hasNextInt()) {
             view.showErrorMessage(View.OUTPUT_FORMAT_ERROR);
-            view.showInputMessage(minOfRange, maxOfRange);
+            view.showInputMessage(model.getMinOfRange(), model.getMaxOfRange());
             scanner.next();
         } else {
             int inputNumber = scanner.nextInt();
-            checkRangeError(inputNumber);
-            view.showInputMessage(minOfRange, maxOfRange);
+            model.checkRangeError(inputNumber, view);
+            view.showInputMessage(model.getMinOfRange(), model.getMaxOfRange());
         }
     }
-
-    /**
-     * Check if input number is not out of search range
-     * */
-    private void checkRangeError(int inputNumber) {
-        if (inputNumber < minOfRange || inputNumber > maxOfRange) {
-            view.showErrorMessage(View.OUT_OF_RANGE_ERROR);
-        } else {
-            checkInputBeforeError(inputNumber);
-            changeMaxRange(inputNumber);
-            changeMinRange(inputNumber);
-        }
-    }
-
-    /**
-     * Check if number is not being wrote before to statistic
-     * */
-    private void checkInputBeforeError(int inputNumber) {
-        ArrayList<Integer> statistic = model.getStatistic();
-        if (statistic.contains(inputNumber)) {
-            view.showErrorMessage(View.INPUT_BEFORE_ERROR);
-        } else {
-            model.addNumberToStatistic(inputNumber);
-        }
-    }
-
-    /**
-     * Change maximum threshold of range if input number bigger than searchable number
-     * */
-    private void changeMaxRange(int inputNumber){
-        if (inputNumber > number) {
-            maxOfRange = inputNumber;
-        }
-    }
-
-    /**
-     * Change minimum threshold of range if input number less than searchable number
-     * */
-    private void changeMinRange(int inputNumber){
-        if (inputNumber < number) {
-            minOfRange = inputNumber;
-        }
-    }
+    
 }
