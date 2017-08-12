c55733121e9394c8afa1cdfb2b175957837b831c
Dimagio <sdmitriy3758@gmail.com>
2017-08-12 17:14:27 +0300

    update stock

--- src/main/java/main/java/task5/fifth_question/stocks/Stock.java
+++ src/main/java/main/java/task5/fifth_question/stocks/Stock.java
@@ -1,11 +1,13 @@
 package main.java.task5.fifth_question.stocks;
 
+import main.java.task5.fifth_question.Ingredient;
+
 import java.util.List;
 
 /**
  * Created by troll on 11.08.2017.
  */
-public abstract class Stock<T> {
+public abstract class Stock<T extends Ingredient> {
 
     List<T> ingredientsInStock;
 
